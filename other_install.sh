d63e58777bf8d637be1f5a14cebee7c3320f9e99
aaf <aaf@debian.lan>
2016-12-10 09:45:37 +0100

    conky

---
+++ other_install.sh
@@ -0,0 +1,20 @@
+#/bin/bash
+
+GITFOLDER="$HOME/configfiles/other"
+
+conky(){
+RCFILE="$HOME/.conkyrc"
+
+mv $RCFILE $RCFILE.bck
+ln -s $GITFOLDER/conkyrc $RCFILE
+}
+
+dmenu(){
+RCFILE="$HOME/.config/dmenu/dmenu-bind.sh"
+
+mv $RCFILE $RCFILE.bck
+ln -s $GITFOLDER/dmenu-bind.sh $RCFILE
+}
+
+conky
+dmenu
